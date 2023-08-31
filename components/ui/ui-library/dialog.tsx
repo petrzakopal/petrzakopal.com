cdc9f9e9261c7d559d573ee06434c56b0a7e9a95
petrzakopal <petr.zakopal@gmail.com>
2023-08-31 14:23:13 +0200

    edited dialog component to enable/disable animations

--- components/ui/ui-library/dialog.tsx
+++ components/ui/ui-library/dialog.tsx
@@ -10,6 +10,8 @@ const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
+const animate = false;
+
 const DialogPortal = ({
 	className,
 	...props
@@ -25,7 +27,10 @@ const DialogOverlay = React.forwardRef<
 	<DialogPrimitive.Overlay
 		ref={ref}
 		className={ctw(
-			"fixed inset-0 z-50 bg-light-white/80 dark:bg-dark-black/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+			"fixed inset-0 z-50 bg-light-white/80 dark:bg-dark-black/80 backdrop-blur-sm",
+			animate
+				? "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+				: "",
 			className
 		)}
 		{...props}
@@ -42,7 +47,10 @@ const DialogContent = React.forwardRef<
 		<DialogPrimitive.Content
 			ref={ref}
 			className={ctw(
-				"fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-zinc-200 dark:border-neutral-900 bg-light-white dark:bg-dark-black p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
+				"fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-zinc-200 dark:border-neutral-900 bg-light-white dark:bg-dark-black p-6 shadow-lg sm:rounded-lg md:w-full",
+				animate
+					? "duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]"
+					: "",
 				className
 			)}
 			{...props}
